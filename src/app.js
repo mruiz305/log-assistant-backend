32c80648b8cb00dee23331979b2c11e4a695e6ca
mruiz305 <mruiz@305nofault.com>
2026-01-28 18:34:28 -0600

    CORS

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); 
+app.options('/.*/', cors(corsOptions)); 
 // 4) body limit (solo una vez)
 app.use(express.json({ limit: '64kb' }));
 
